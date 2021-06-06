6dddfc36c4e36336eb285b77da35e1532ae7c04e
karasansar <55123100+dogukan-karasansar@users.noreply.github.com>
2021-06-07 00:13:04 +0300

    service, page and uml added

---
+++ src/components/Navigate.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navigate = () => {
+  return <div>Navigation</div>;
+};
+
+export default Navigate;
---
+++ src/services/school/SchoolService.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+class SchoolService {}
+
+export default SchoolService;
